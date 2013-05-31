99250585b590637a1ab98c64223b141431179044
Andrzejster <andrzejster@extreme-fusion.pl>
2013-05-31 19:08:40 +0200

    Added Czech local files

---
+++ modules/praises/locale/Czech/admin/admin.php
@@ -0,0 +1,23 @@
+<?php defined('EF5_SYSTEM') || exit;
+
+return array(
+	'Praises' => 'Pochvaly',
+	'User Name' => 'Uživatel',
+	'Reason' => 'Důvod',
+	//Logs
+	'Added praise' => 'Přidána pochvala',
+	'Fails to add praise' => 'Chyba během přidávání pochvaly',
+	'Crash deleting praise' => 'Chyba během mazání pochvaly',
+	'Deleted praise' => 'Smazána pochvala',
+	'Edited praise' => 'Upravena pochvala',
+	'Error when editing a praise' => 'Chyba během úpravy pochvaly',
+	'Praise has been removed' => 'Pochvala byla smazána',
+	'Praise has been added' => 'Pochvala byla přidána',
+	'Crash when adding a praise' => 'Chyba během přidávání pochvaly',
+	'Crash when removing a praise' => 'Chyba během mazání pochvaly',
+	'No ID :id for the table praises.' => 'Chyba ID :id pro tabulku pochvaly',
+	//Private Message
+	'Send a private message to user' => 'Poslat soukromou zprávu uživateli',
+	'Received Praise' => 'Přijaté pochvaly',
+	'Administrator has given you praise as follows: :praise' => 'Administrátor Vám dal pochvalu s následujícím obsahem: :praise',
+);
---
+++ modules/praises/locale/Czech/lang.php
@@ -0,0 +1,10 @@
+<?php defined('EF5_SYSTEM') || exit;
+
+return array(
+	'Praises' => 'Pochvaly',
+	'Praises user :username' => 'Pochvala uživatele :username',
+	'This plugin allows you to add praises to users.' => 'Tento modul umožňuje uživatelům přidávat pochvaly.',
+	//Profile
+	'You have :how praises' => 'Přijatá pochvala: :how',
+	'Back to profile' => 'Zpět do profilu',
+);
